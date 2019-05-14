c88f1861046ca3186a17b0bb50d3c28095968367
shoyebinamdar <shoyeb.inamdar@volkswagen.co.in>
2019-05-14 11:41:18 +0530

    Added Structural:Facade pattern example

---
+++ src/com/shofee/behavioural/Facade.java
@@ -0,0 +1,47 @@
+package com.shofee.behavioural;
+
+class A {
+    public void methodA() {
+        System.out.println("Method A");
+    }
+}
+
+class B {
+    public void methodB() {
+        System.out.println("Method B");
+    }
+}
+
+class C {
+    public void methodC() {
+        System.out.println("Method C");
+    }
+}
+
+// Main interface class to hide the background complexity
+class FacadeWrapper{
+    A a;
+    B b;
+    C c;
+
+    FacadeWrapper() {
+        a = new A();
+        b = new B();
+        c = new C();
+    }
+
+    public void compute() {
+        a.methodA();
+        b.methodB();
+        c.methodC();
+    }
+
+}
+
+public class Facade {
+    public static void main(String args[]) {
+        // here we just call the compute method of FacadeWrapper which in turn takes care of the rest
+        FacadeWrapper facadeWrapper = new FacadeWrapper();
+        facadeWrapper.compute();
+    }
+}
